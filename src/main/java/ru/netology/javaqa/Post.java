edffc1b7e9c3a3a3cb6e1aa0d62523433eb59d0d
Neto test <vovka9313@mail.ru>
2023-07-08 10:20:24 +0300

    homework 9.1

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.javaqa;
+
+public class Post {
+    public String name; // имя
+    public String patronymic; // отчество
+    public String surname; // фамилия
+    public boolean subscription; // подписка
+    public String passport; // паспорт
+    public String phone; // телефон
+    FormDate birthday = new FormDate();
+}
